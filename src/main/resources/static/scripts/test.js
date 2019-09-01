c8e85c37f73b913ac61888a8536a35b70ebc91b1
PotapenkoAS <PotapenkoAC@gmail.com>
2019-09-01 23:30:42 +0300

    make medicaments on the test page show on click

--- src/main/resources/static/scripts/test.js
+++ src/main/resources/static/scripts/test.js
@@ -1,4 +1,5 @@
 var symptoms = [];
+var id = Number(0);
 
 function updateDiseases() {
     // создание ajax запроса
@@ -10,13 +11,13 @@ function updateDiseases() {
             var j = -1;
             var arr = [];
             //обнуление списка болезней если данных не вернулось
-            if(data==null){
+            if (data == null) {
                 $('#diseasesList').html("");
                 return;
             }
             //генерация нового контента по пришедшим данным
             for (var i = 0; i < data.length; i++) {
-                arr[++j] = '<div>';
+                arr[++j] = '<div id="d' + id + '" onclick="swapVisibility(this.id)">';
                 arr[++j] = '<div><p>Название: ' + data[i].diseaseName + '</p><p>Описание: ' + data[i].diseaseInfo + '</p></div>';
                 arr[++j] = '<div><p>Вероятность по обязательным симптомам: ' + data[i].mandatoryRate + '</p>';
                 if (!!data[i].optionalRate) {
@@ -24,12 +25,13 @@ function updateDiseases() {
                 }
                 arr[++j] = '</div><br>';
 
-                arr[++j] = '<div>';
+                arr[++j] = '<div id="m' + id + '" style="display: none">';
                 for (var k = 0; k < data[i].meds.length; k++) {
                     arr[++j] = '<p>Лекарство: ' + data[i].meds[k].name + '</p><p>Описание: ' + data[i].meds[k].info + '</p><p>Рейтинг: ' + data[i].meds[k].rate + '</p>';
                 }
                 arr[++j] = '</div>';
                 arr[++j] = '</div><br>';
+                id++;
             }
             //добавление сгенерированного контента на страницу
             $('#diseasesList').html(arr.join(''));
@@ -59,6 +61,7 @@ function saveTest() {
         })
     }
 }
+
 // изменение списка симпотомов и обновление списка болезней
 function updateSymptoms(checkbox) {
     document.getElementById("check_mark").innerText = "";
@@ -71,4 +74,13 @@ function updateSymptoms(checkbox) {
         })
     }
     updateDiseases();
+}
+
+function swapVisibility(elId) {
+    var trueElId = "m" + elId.substr(1, elId.length - 1);
+    if (document.getElementById(trueElId).style.display === "none") {
+        document.getElementById(trueElId).style.display = "block";
+    } else {
+        document.getElementById(trueElId).style.display = "none";
+    }
 }
